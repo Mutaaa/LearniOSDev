9e7d6f95c6e3654c35eb3bc8a7acd7354dd022f9
Kornkanok Sangwichien <mu.nsnun@gmail.com>
2018-11-12 15:49:12 +0700

    first app ios

--- Element Quiz v0.1/ViewController.swift
+++ Element Quiz v0.1/ViewController.swift
@@ -9,10 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var ansLabel: UILabel!
+    
+    
+    @IBAction func showAns(_ sender: Any) {
+        ansLabel.text = elementList[currentElementIndex]
+    }
+    
+    @IBAction func nextToElement(_ sender: Any) {
+        currentElementIndex += 1
+        if currentElementIndex >= elementList.count {
+            currentElementIndex = 0
+        }
+        updateElement()
+        
+        
+    }
+    
+    
+    let elementList = ["Carbon", "Gold", "Chlorine", "Sodium"]
+    var currentElementIndex = 0
+    
+    func updateElement(){
+        ansLabel.text = "?"
+        imgView.image = UIImage(named: elementList[currentElementIndex])
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateElement()
     }
 
 
