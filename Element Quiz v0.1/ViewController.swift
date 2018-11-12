7e3300aff945ac3ee58c9377876df37e55db7a5a
Kornkanok Sangwichien <mu.nsnun@gmail.com>
2018-11-12 14:15:09 +0700

    Initial Commit

---
+++ Element Quiz v0.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Element Quiz v0.1
+//
+//  Created by Kornkanok Sangwichien on 12/11/2561 BE.
+//  Copyright © 2561 Kornkanok Sangwichien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
